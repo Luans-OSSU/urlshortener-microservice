9c39b5f6637977bdb693f29a9a0ca8178da625b3
Luans <luansnain@gmail.com>
2018-12-08 13:58:58 +0100

    init server

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+var cors = require('cors');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/ 
+// mongoose.connect(process.env.MONGOLAB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+  
+
+app.listen(port, function () {
+  console.log('Node.js listening on port ' + port);
+});
